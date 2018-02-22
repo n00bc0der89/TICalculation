37df06c73f96220dae0e2a7e789d4e8dffdd7fe4
Sachin Shetty <scn.3089@gmail.com>
2018-02-22 11:56:25 +0530

    Add files via upload

--- router/main.js
+++ router/main.js
@@ -1,67 +1,146 @@
 'use strict'
-module.exports = function(app){
-var bodyParser = require("body-parser");
-var http = require('http');
-var kafka = require('kafka-node');
-var Consumer = kafka.Consumer;
-var Producer = kafka.Producer;
+module.exports = function(app) {
+    var bodyParser = require("body-parser");
+    var http = require('http');
+    var kafka = require('kafka-node');
+    var Consumer = kafka.Consumer;
+    var Producer = kafka.Producer;
+    var SSE = require('sse-nodejs');
+    var client = new kafka.Client('172.16.243.118:5181');
+    var producer = new Producer(client);
+    var Q = require("q");
+    var async = require("async");
+    let adjvalues = 0; 
 
-var client = new kafka.Client('localhost:2181');
-var producer = new Producer(client);
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.urlencoded({extended : true}));
-    
-app.get("/TIScreen",function(req,res){
-    
-    
-    res.render("index"); 
-    
-});   
+    app.get("/TIScreen", function(req, res) {
 
-app.post("/calculateTI",function(req,res){
-    let questioncode = req.body.questioncode;
-    let adjvalues = req.body.countrycodevalues;
-    
-    let data = {};
-    let resilientvalue = 0;
-    let competitivevalue = 0;
-    
-    //Pass question code to kafka topic and multiply with adjvalues and print on screen
-    var payloads = [{topic: 'TItopics', messages: questioncode, partition: 0 }];
-    
-     producer.send(payloads, function (err, data) {
-		console.log('Pushed Successfully');
-		
-		//some delay and then read back from another topic
-		
-		setTimeout(fromConsumer, 2000);
-		function fromConsumer(){
-        	 var consumer = new Consumer(
-                    client,
-                    [
-                        { topic: 'TItopics2', partition: 0 }
-                    ],
-                    {
-                        autoCommit: false
+
+        res.render("index");
+
+    });
+
+    app.post("/calculateTI", function(req, res) {
+        console.log("Called calculateTI function");
+        let questioncode = req.body.questioncode;
+        adjvalues = req.body.countrycodevalues;
+
+        let data = {};
+        let resilientvalue = 0;
+        let competitivevalue = 0;
+
+        //Pass question code to kafka topic and multiply with adjvalues and print on screen
+        var payloads = [{ topic: 'TItopics3', messages: questioncode, partition: 0 }];
+
+        console.log("Payloads is : " + JSON.stringify(payloads));
+
+        producer.send(payloads, function(err, data) {
+            console.log('Pushed Successfully');
+
+            //some delay and then read back from another topic
+
+            //setTimeout(fromConsumer, 5000);
+
+            /*  function fromConsumer(){
+                       
+                       const options = {
+                            autoCommit: true,
+                            fetchMaxWaitMs: 1000,
+                            fetchMaxBytes: 1024 * 1024,
+                            encoding: "buffer"
+                        };
+
+                        const topics = [
+                            {
+                                topic: "TItopics4"
+                            }
+                        ];
+
+                       var consumer = new kafka.HighLevelConsumer(client,topics, options);
+                        
+                        consumer.on('message', function (message) {
+                            var buf = new Buffer(message.value, "binary"); 
+                            var decodedMessage = JSON.parse(buf.toString());
+                            console.log(decodedMessage);
+                            //Get json response from kafa topics
+                          //  var responseobj = JSON.parse(message);
+                          //  resilientvalue = responseobj.resilientvalue * adjvalues;  // Kafka topic must generate json response as {resilientvalue : <somevalue>, competitivevalue : <somevalue>}
+                          //  competitivevalue = responseobj.competitivevalue * adjvalues;
+                            resilientvalue = decodedMessage;
+                            data  = {"Resilient" : resilientvalue , "Competitive" : competitivevalue};
+                            console.log("Sending data to screen");
+                            res.send(data);
+                        });
+
+                    
+                }
+                //fromConsumer();
+                */
+        });
+
+    });
+
+    app.get("/getTIvalues", function(req, res) {
+
+        var app1 = SSE(res);
+
+        setInterval(
+            function() {
+                async.series([
+                    function(cb) {
+                        const options = {
+                           /* autoCommit: true,
+                            fetchMaxWaitMs: 1000,
+                            fetchMaxBytes: 1024 * 1024,
+                            encoding: "buffer" */
+                            autoCommit: true
+                        };
+
+                         const topics = [{
+                                        topic: "TItopics4" , partition: 0
+                                    }];
+
+                    
+                          var consumer = new kafka.HighLevelConsumer(client, topics, options);
+
+                            consumer.once('message', function(message) {
+                               // var buf = new Buffer(message.value, "binary");
+                               // var decodedMessage = JSON.parse(buf.toString());
+                               var decodedMessage = message.value;
+                                console.log(decodedMessage);
+                                //Get json response from kafa topics
+                                //  var responseobj = JSON.parse(message);
+                                //  resilientvalue = responseobj.resilientvalue * adjvalues;  // Kafka topic must generate json response as {resilientvalue : <somevalue>, competitivevalue : <somevalue>}
+                                //  competitivevalue = responseobj.competitivevalue * adjvalues;
+                                cb(null, decodedMessage);
+                                // return decodedMessage;
+                            });
+
+                        
                     }
-                );
-                
-                consumer.on('message', function (message) {
-                    //console.log(message);
-                    //Get json response from kafa topics
-                    var responseobj = JSON.parse(message);
-                    resilientvalue = responseobj.resilientvalue * adjvalues;  // Kafka topic must generate json response as {resilientvalue : <somevalue>, competitivevalue : <somevalue>}
-                    competitivevalue = responseobj.competitivevalue * adjvalues;
-                    
-                    data  = {"Resilient" : resilientvalue , "Competitive" : competitivevalue};
-                    
-                    res.send(data);
-                });
-		}
-		
-     });
-    
-});  
-    
-}
+                ], function(err, data) {
+                    console.log(err);
+                    console.log('data', data);
+                    console.log("Adjvalues", adjvalues);
+                    data = data *adjvalues;
 
+                    app1.sendEvent('getTIvalues', function() {
+                        return data;
+                    });
+                });
+
+            }, 1000);
+
+
+
+
+        /*kafkaConsumer().then(function(scorevalue){
+            console.log("From sse : " + scorevalue);
+            console.log("Response is : "+ res);
+            
+        }); */
+
+    });
+
+}
